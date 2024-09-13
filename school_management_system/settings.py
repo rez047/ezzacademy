667e19ae3f9e5b60bf5692c024569e39115d138c
rez047 <113913401+rez047@users.noreply.github.com>
2024-09-13 14:58:25 +0300

    Update settings.py

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -1,4 +1,7 @@
 import os
+import dj_database_url
+import django_heroku
+django_heroku.settings(locals())
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -68,17 +71,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'school_management_system.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-    }
+    'default': dj_database_url.config(default='postgres://localhost')
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
